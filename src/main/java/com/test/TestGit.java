a333e2b16597cc15903cfd90b9f8aedd1d05b7b1
Bhavani Ragunathan <bhavaniragunathan@Bhavanis-MacBook-Pro.local>
2022-09-24 12:30:07 +0100

    First Commit

--- src/main/java/com/test/TestGit.java
+++ src/main/java/com/test/TestGit.java
@@ -5,4 +5,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test Git");
     }
+    public void calc()
+    {
+        System.out.println("Sum=" +(5+3));
+    }
 }
